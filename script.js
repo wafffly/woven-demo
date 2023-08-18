a7c0104bb7d0a6fa90ffb08eba787948c5f68a9a
desslinga <dess.linga@gmail.com>
2023-08-18 04:07:55 -0400

    Add 'add clothing' view and changing some styles

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
 let viewClothesContainerElement = document.getElementById('view-clothes-container');
 let clothesContainerElement = document.getElementById('clothes-container');
+let addClothingButton = document.getElementById('add-clothing');
+
+function loadViewClothesView() {
+    document.getElementById('main-page-container').classList.add('show');
+    setTimeout(() => {
+        document.getElementById('main-page-container').classList.add('animated');
+    }, 50);
+
+    $('#main-page-container').load('pages/view-clothes-container.html', () => {
+        // DOM elements
+        viewClothesContainerElement = document.getElementById('view-clothes-container');
+        clothesContainerElement = document.getElementById('clothes-container');
+        addClothingButton = document.getElementById('add-clothing');
+
+        // Event handlers
+        addClothingButton.addEventListener('click', loadAddClothingView);
+
+        // Initialize main page by populating clothes
+        populateClothes();
+    });
+}
+
+function loadAddClothingView() {
+    $('#side-page-container').load('pages/add-clothing-container.html', () => {
+        document.getElementById('side-page-container').classList.add('show');
+        setTimeout(() => {
+            document.getElementById('side-page-container').classList.add('animated');
+        }, 50);
+
+        // DOM elements
+        closeAddClothingContainerButton = document.getElementById('close-add-clothing-container');
+
+        // Event handlers
+        closeAddClothingContainerButton.addEventListener('click', closeAddClothingView);
+    });
+}
+
+function closeAddClothingView() {
+    const sidePageContainerElement = document.getElementById('side-page-container');
+    sidePageContainerElement.innerHTML = '';
+    sidePageContainerElement.classList.remove('animated');
+    setTimeout(() => {
+        document.getElementById('side-page-container').classList.remove('show');
+    }, 300);
+}
 
 // fetch the JSON clothes and display to DOM
-function loadViewClothesContainer() {
+function populateClothes() {
     fetch('data/clothing.json')
         .then(res => res.json())
         .then(data => {
@@ -31,16 +76,10 @@ function createClothingElement(clothing) {
             </div>
         </div>
     `;
-
     return clothingElement;
 }
 
-
 // Dynamically load content to page container
 $(document).ready(function() {
-    $('#view-clothes-page-container').load('pages/view-clothes-container.html', () => {
-        viewClothesContainerElement = document.getElementById('view-clothes-container');
-        clothesContainerElement = document.getElementById('clothes-container');
-        loadViewClothesContainer();
-    });
+    loadViewClothesView();
 })
