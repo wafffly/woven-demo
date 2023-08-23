afb7fe56c95abd4ea298dd979a7d97082ebaeeef
desslinga <dess.linga@gmail.com>
2023-08-23 16:58:54 -0400

    Small refactor in view-clothing module

--- scripts/view-clothing.js
+++ scripts/view-clothing.js
@@ -2,35 +2,26 @@ import {
     domSelect
 } from './utils.js';
 
+const clothingFieldsConfig = {
+    brand: { title: 'Brand' },
+    title: { title: 'Title' },
+    color: { title: 'Color' },
+    category: { title: 'Category' },
+    price: { title: 'Price' },
+    worn: { title: 'Number of Wears' }
+};
+
 const loadViewClothingView = clothing => {
     $("#side-page-container").load('pages/view-clothing-container.html', () => {
         openSidePageContainer();
 
         domSelect('view-clothing-img').src = clothing.imageFile;
-        domSelect('view-clothing-brand').innerHTML = `
-            <span class='label'>Brand</span>
-            <span class='value'>${clothing.brand}</span>
-        `;
-        domSelect('view-clothing-title').innerHTML = `
-            <span class='label'>Title</span>
-            <span class='value'>${clothing.title}</span>
-        `;
-        domSelect('view-clothing-color').innerHTML = `
-            <span class='label'>Color</span>
-            <span class='value'>${clothing.color}</span>
-        `;
-        domSelect('view-clothing-category').innerHTML = `
-            <span class='label'>Category</span>
-            <span class='value'>${clothing.category}</span>
-        `;
-        domSelect('view-clothing-price').innerHTML = `
-            <span class='label'>Price</span>
-            <span class='value'>${clothing.price}</span>
-        `;
-        domSelect('view-clothing-worn').innerHTML = `
-            <span class='label'>Number of Wears</span>
-            <span class='value'>${clothing.worn}</span>
-        `;
+        Object.entries(clothingFieldsConfig).forEach(([id, { title }]) => {
+            domSelect(`view-clothing-${id}`).innerHTML = `
+                <span class='label'>${title}</span>
+                <span class='value'>${clothing[id]}</span>
+            `;
+        });
 
         // Event handlers
         domSelect('close-view-clothing-container').addEventListener('click', handleClickCloseSidePageContainer);
